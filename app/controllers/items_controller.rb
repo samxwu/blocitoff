5054d0a1648d05905f45f91bba2f1b34fc3907ff
Brittany Martin <brittany.jill.martin@gmail.com>
2017-04-10 17:07:09 -0400

    Fix Syntax Issue in ItemsController, add FadeOut to destroy.js.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,57 +1,35 @@
 class ItemsController < ApplicationController
-  
-#  def new
-#    @item = Item.new
-#  end
 
   def create
-  #  @item = Item.new
-  #  @item.name = params[:item][:name]
-  #  @item.user = current_user
 
-  #respond_to do |format|
-  #  if @item.save
-  #    format.html { redirect_to user_path(current_user.id), notice: 'Item was successfully saved.'}
-  #    format.js
-  #  else
-  #    format.html { redirect_to user_path(current_user.id), notice: 'Error creating item. Please try again.'}
-  #  end
-  #end
-  
-  
     @item = Item.new
     @item.name = params[:item][:name]
     @item.user = current_user
-    
+
     if @item.save
-        flash[:notice] = "Item was saved successfully."
-        redirect_to user_path(current_user.id)
-        
+      flash[:notice] = "Item was saved successfully."
+      redirect_to user_path(current_user.id)
     else
-        flash.now[:alert] = "Error creating item. Please try again."
-        redirect_to user_path(current_user.id)
+      flash.now[:alert] = "Error creating item. Please try again."
+      redirect_to user_path(current_user.id)
     end
-
   end
-  
-  
-  
+
+
   def destroy
-      
-      @user = current_user
-      @item = @user.items.find(params[:id])
-      
-    respond_to do |format|
-      if @item.destroy
-        format.html { redirect_to @user, notice: 'Item was deleted.'}
-        format.js
-      else
-        format.html { redirect_to @user, notice: 'Error deleting item. Please try again.'}
-        format.js
-      end
+    @user = current_user
+    @item = @user.items.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Item has been completed!"
+    else
+      flash[:alert] = "There was an error completing the item. Try again."
     end
 
-  
-  
-  
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
 end
