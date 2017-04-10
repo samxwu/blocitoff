a142b4896ff7afbee3b85110202ae3862982cd19
Sam Wu <sam.x.wu@gmail.com>
2017-04-10 18:31:21 +0000

    Checkpoint 7 Mark Do To As Complete - Commit 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
   
-  def new
-    @item = Item.new
-  end
-  
+#  def new
+#    @item = Item.new
+#  end
+
   def create
+  #  @item = Item.new
+  #  @item.name = params[:item][:name]
+  #  @item.user = current_user
+
+  #respond_to do |format|
+  #  if @item.save
+  #    format.html { redirect_to user_path(current_user.id), notice: 'Item was successfully saved.'}
+  #    format.js
+  #  else
+  #    format.html { redirect_to user_path(current_user.id), notice: 'Error creating item. Please try again.'}
+  #  end
+  #end
+  
+  
     @item = Item.new
     @item.name = params[:item][:name]
     @item.user = current_user
@@ -19,6 +33,24 @@ class ItemsController < ApplicationController
     end
 
   end
+  
+  
+  
+  def destroy
+      
+      @user = current_user
+      @item = @user.items.find(params[:id])
+      
+    respond_to do |format|
+      if @item.destroy
+        format.html { redirect_to @user, notice: 'Item was deleted.'}
+        format.js
+      else
+        format.html { redirect_to @user, notice: 'Error deleting item. Please try again.'}
+        format.js
+      end
+    end
+
   
   
   
